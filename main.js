409c6c91d616d699f5b4b9f3686e4e11e3ff0f2e
Bobir <akmalist@gmail.com>
2021-08-30 13:48:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@
     var cityName = document.getElementById('cityName').value;
     console.log(cityName);
     
-    var key = '89a75e05a8fb64556b3c3f00e5032c20';
-    var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=89a75e05a8fb64556b3c3f00e5032c20";
+    var key = ' ';
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid= ";
     fetch(url)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
